1531eeda7dd22c62e7a691262f10d9cb24c01df9
Ankit Ranjan <me@ankit.io>
2014-08-30 22:17:31 -0700

    Added registration count

--- app/app.py
+++ app/app.py
@@ -8,7 +8,13 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    try:
+        with open('../registrations.csv') as f:
+            registrants = sum(1 for _ in f) - 1
+    except IOError:
+        # :O
+        registrants = 420
+    return render_template('main.html', registrants=registrants)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))

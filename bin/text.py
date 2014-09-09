7eec377efda1052bb5dc63913681603880ea7a99
Ankit Ranjan <me@ankit.io>
2014-09-08 17:21:32 -0700

    Better texts

--- bin/text.py
+++ bin/text.py
@@ -16,10 +16,9 @@ with open('../../randomized.csv') as f:
 			users.append(row)
 	finally:
 		f.close()      # closing
-users = users[1:] #get rid of title
 targets = users[1:] + users[:1]
 
- 
+# Omitted in commit 
 ACCOUNT_SID = ""
 AUTH_TOKEN = "" 
 
@@ -30,8 +29,11 @@ for i in xrange(len(users)):
 	number = users[i][3]
 	target = targets[i][0]
 	if number:
+		print 'Texting %s' % user
 		client.messages.create(
 			to=number,
 			from_="+16502414303",
 			body="Good evening Agent %s. Your mission, if you choose to accept it, is to eliminate %s. Contact the GM if you need more info. Good luck!" % (user, target)
 		)
+	else:
+		print 'Not texting %s' % user
